d028d4a13abe5e480bec9e697d263acd5548ddcc
JDominguez12 <144716213+JDominguez12@users.noreply.github.com>
2026-08-26 22:29:27 -0700

    adding file

---
+++ sortingAlgorithms/BubbleSort.py
@@ -0,0 +1,17 @@
+def bubblesort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i -1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+
+arr = [23, 64, 12, 35, 62]
+print("Original Array: ", arr)
+arr = bubblesort(arr)
+print("Sorted Array: ", arr)
